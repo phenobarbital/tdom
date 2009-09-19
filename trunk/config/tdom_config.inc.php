41918b49308fb024054bf06f46e3e2cfe1aa82c7
jesuslarag@gmail.com <jesuslarag@gmail.com@3dbcaa76-a4c4-11de-bb10-370f7287a6b7>
2009-09-19 03:40:11 +0000

    tdom: added version() method tdom_config added config file for TDOM

---
+++ trunk/config/tdom_config.inc.php
@@ -0,0 +1,17 @@
+<?php
+#la version actual de tomato TDOM
+define('TDOM_VERSION', '0.1.2');
+#relativas al encoding y version del XML
+define('XML_VERSION', '1.0');
+define('XML_LOCAL_VALIDATION', true); #causa una validacion local del XML
+define('XML_ENCODING', 'utf-8');
+define('STANDALONE', false); #required for some applications
+define('XML_STRICT', true); #xhtml 1.0 strict or xhtml transitional?
+define('XML_LANG', 'es'); #lenguaje para el constructor xhtml
+define('XML_FORMAT', true); #permite formatear la salida del DOM
+#propiedades relativas al URI
+define('XML_HTTP_PORT', ''); #puerto, cambiar si es distinto de 80 (ej. :8080)
+define('XML_HTTP_PROTOCOL', 'http'); #por defecto http (https|rss|file)
+#ruta de inclusion de DTD y otros archivos (relativa a la ruta absoluta de la aplicacion)
+define('XML_INCLUDE', 'tomates/include/tdom');
+?>
